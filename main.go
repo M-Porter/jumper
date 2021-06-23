e2ea3896dccce3b65ba66b730e3db26008bff887
Matt Porter <mtp5129@gmail.com>
2021-06-23 16:36:47 -0700

    move variables around a bit

--- main.go
+++ main.go
@@ -7,39 +7,37 @@ import (
 	"os"
 )
 
-func executeCmdTo(cmd *cobra.Command, args []string) error {
-	return core.Run(args)
+// jumper
+var rootCmd = &cobra.Command{
+	Use:   "jumper",
+	Short: "Seamlessly jump between projects on your machine.",
 }
 
-func executeCmdAnalyze(cmd *cobra.Command, args []string) {
-	core.Analyze()
+// jumper to
+var toCmd = &cobra.Command{
+	Use:   "to",
+	Short: "Run the jumper TUI.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return core.Run(args)
+	},
+}
+
+// jumper analyze
+var analyzeCmd = &cobra.Command{
+	Use:   "analyze",
+	Short: "Analyze and cache projects.",
+	Run: func(cmd *cobra.Command, args []string) {
+		core.Analyze()
+	},
 }
 
 func main() {
 	cobra.OnInitialize(core.Init)
 
-	jumperCmd := &cobra.Command{
-		Use:   "jumper",
-		Short: "Seamlessly jump between projects on your machine.",
-	}
-
-	toCmd := &cobra.Command{
-		Use:   "to",
-		Short: "Run the jumper TUI.",
-		RunE:  executeCmdTo,
-	}
-
-	analyzeCmd := &cobra.Command{
-		Use:   "analyze",
-		Short: "Analyze and cache projects.",
-		Run:   executeCmdAnalyze,
-	}
-
 	// todo: install cmd
+	rootCmd.AddCommand(toCmd, analyzeCmd)
 
-	jumperCmd.AddCommand(toCmd, analyzeCmd)
-
-	if err := jumperCmd.Execute(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
