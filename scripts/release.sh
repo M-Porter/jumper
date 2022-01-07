d23b5e32422efa4dd1aa8d9ba06f879de8d24876
Matt Porter <mtp5129@gmail.com>
2022-01-07 16:55:47 -0700

    update script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,18 @@ set -u
 TAG_VERSION="$1"
 COMMIT_MESSAGE="$2"
 
+git ls-remote --exit-code --tags origin "$TAG_VERSION"
+if [[ "$?" == "0" ]]; then
+    echo "ERROR! Remote tag $TAG_VERSION already exists."
+    echo "Exiting."
+    exit 1
+fi
+
 git tag -a "$TAG_VERSION" -m "$COMMIT_MESSAGE"
 git push origin "$TAG_VERSION"
 
 goreleaser release
+
+# can delete tags with
+#   git tag -d "$TAG_VERSION"
+#   git push --delete origin "$TAG_VERSION"
