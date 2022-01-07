3d4de46a247eec7df7df39d82298b8e1ac978998
Matt Porter <mtp5129@gmail.com>
2022-01-07 16:57:41 -0700

    more

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,13 +8,11 @@ COMMIT_MESSAGE="$2"
 
 git ls-remote --exit-code --tags origin "$TAG_VERSION"
 if [[ "$?" == "0" ]]; then
-    echo "ERROR! Remote tag $TAG_VERSION already exists."
-    echo "Exiting."
-    exit 1
+    echo "Warning! Remote tag $TAG_VERSION already exists."
+else
+    git tag -a "$TAG_VERSION" -m "$COMMIT_MESSAGE"
+    git push origin "$TAG_VERSION"
 fi
-
-git tag -a "$TAG_VERSION" -m "$COMMIT_MESSAGE"
-git push origin "$TAG_VERSION"
 
 goreleaser release
 
