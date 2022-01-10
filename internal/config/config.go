4873de1c40e0c87bf91766a13786dce232562111
Matt Porter <mtp5129@gmail.com>
2022-01-10 11:28:02 -0700

    path stops from config now working

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"fmt"
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 const (
@@ -16,13 +18,13 @@ const (
 )
 
 var (
-	defaultIncludes = []string{
+	defaultSearchIncludes = []string{
 		"development/",
 		"dev/",
 		"xcode-projects/",
 		"repos/",
 	}
-	defaultExcludes = []string{
+	defaultSearchExcludes = []string{
 		"/node_modules",
 		"/bin",
 		"/temp",
@@ -31,15 +33,35 @@ var (
 		"/venv",
 		"/ios/Pods",
 	}
+	defaultSearchPathStops = []string{
+		"/.git",
+		"/Gemfile",
+		"/package.json",
+		"/go.mod",
+		"/setup.py",
+		"/pyproject.toml",
+	}
 )
 
 type Config struct {
 	HomeDir           string
 	JumperDir         string
 	CacheFileFullPath string
-	CacheFile         string   `mapstructure:"cache_file"`
-	SearchIncludes    []string `mapstructure:"search_includes"`
-	SearchExcludes    []string `mapstructure:"search_excludes"`
+	CacheFile         string
+	SearchIncludes    []string
+	SearchExcludes    []string
+	SearchPathStops   []*regexp.Regexp
+}
+
+// the config structure as written to the file
+type configFromFile struct {
+	CacheFile string `mapstructure:"cache_file"`
+	// Which paths to include in the search. The starting points.
+	SearchIncludes []string `mapstructure:"search_includes"`
+	// Which paths to ignore from the search if come across within the search excludes.
+	SearchExcludes []string `mapstructure:"search_excludes"`
+	// how we determine not to go any deeper when walking
+	SearchPathStops []string `mapstructure:"search_path_stops"`
 }
 
 var C *Config = nil
@@ -58,8 +80,9 @@ func Init() {
 	viper.AddConfigPath(configDirFull)
 
 	viper.SetDefault("cache_file", DefaultCacheFile)
-	viper.SetDefault("search_includes", defaultIncludes)
-	viper.SetDefault("search_excludes", defaultExcludes)
+	viper.SetDefault("search_includes", defaultSearchIncludes)
+	viper.SetDefault("search_excludes", defaultSearchExcludes)
+	viper.SetDefault("search_path_stops", defaultSearchPathStops)
 
 	err := viper.SafeWriteConfig()
 	if _, ok := err.(viper.ConfigFileAlreadyExistsError); ok {
@@ -72,8 +95,8 @@ func Init() {
 	err = viper.ReadInConfig()
 	cobra.CheckErr(err)
 
-	C = &Config{}
-	err = viper.Unmarshal(C)
+	internalConf := &configFromFile{}
+	err = viper.Unmarshal(internalConf)
 	cobra.CheckErr(err)
 
 	// write the config after reading and setting defaults in case something
@@ -83,9 +106,21 @@ func Init() {
 		cobra.CheckErr(err)
 	}()
 
-	C.HomeDir = hd
+	// copy internalConf to C
+	C = &Config{
+		HomeDir:        hd,
+		SearchIncludes: internalConf.SearchIncludes,
+		SearchExcludes: internalConf.SearchExcludes,
+		CacheFile:      internalConf.CacheFile,
+	}
+
 	C.CacheFileFullPath = filepath.Join(C.HomeDir, JumperDirname, C.CacheFile)
 	C.JumperDir = filepath.Join(C.HomeDir, JumperDirname)
+
+	for _, pathStop := range internalConf.SearchPathStops {
+		pathStopRegexp := regexp.MustCompile(fmt.Sprintf("%s$", regexp.QuoteMeta(pathStop)))
+		C.SearchPathStops = append(C.SearchPathStops, pathStopRegexp)
+	}
 }
 
 func HomeDir() string {
